4407ce7695f7c21b08fab56dd116428d40050de9
jovanmaksic <makssa@outlook.com>
2019-10-29 20:43:28 +0100

    blog sa tonom zahteva

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+//action creators
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fechPosts = () =>{
+    return async (dispatch) =>{
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response.data
+        })
+    }
+}
+
+export const fechUser =(id) =>{
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        })
+    }
+}
---
+++ src/components/PostList.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {connect} from 'react-redux';
+
+import {fechPosts} from '../actions';
+import UserHeader from './UserHeader';
+
+
+class PostList extends React.Component{
+    componentDidMount(){
+        this.props.fechPosts();
+    }
+
+    renderList(){
+        return this.props.posts.map((post) => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                           <h2>{post.title}</h2> 
+                           <p>{post.body}</p> 
+                        </div>
+                        <UserHeader userId={post.userId} />
+                    </div>                    
+                </div>
+            )
+        })
+    }
+    render(){
+        return <div className="ui relaxed devided list">{this.renderList()}</div>;
+    }
+}
+
+const mapStateToProps = (state) =>{
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps,{
+    fechPosts: fechPosts
+})(PostList);
